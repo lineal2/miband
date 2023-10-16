37f1ed18e3a883ace0144d720775057548b80c7c
lineal2 <ducksgomoo@hotmail.co.uk>
2023-10-16 14:52:24 +0900

    changed

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"))
 });
 
-app.post(".netlify/functions/upload",
+app.post("/upload",
     fileUpload({
         createParentPath: true
     }),
